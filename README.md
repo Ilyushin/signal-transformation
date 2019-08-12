cbc3bd9d0a02df8ec5a592780c6059162ced2878
Eugene Ilyushin <eugene.ilyushin@gmail.com>
2019-08-12 16:22:10 +0300

    Added creating an overlapping dataset

--- README.md
+++ README.md
@@ -2,10 +2,11 @@
 
 
 Helps to transform signal data like speech into different representations using TensorFlow API.
-```
+```bash
 pip3 install signal-transformation
 ```
 
+1) Transform wav files to PCM and than save them to tf_records
 ```python
 import tensorflow as tf
 from signal_transformation import tf_transformation
@@ -21,5 +22,23 @@ with tf.compat.v1.Session() as sess:
         audio_path=source_path,
         out_path=result_path
     )
+
+```
+
+2) Create overlapping signals dataset
+```python
+from signal_transformation import helpers
+
+
+source_path = '/path/to/wav/files'
+result_path = '/output/directory'
+
+result = helpers.prepare_data(
+    input_folder,
+    output_folder,
+    size=100,
+    speakers_number=2,
+    overlapping=True
+)
 
 ```
--- signal_transformation/helpers.py
+++ signal_transformation/helpers.py
@@ -1,6 +1,8 @@
 import os
-import pydub
 from glob import glob
+from itertools import permutations
+import pickle
+from pydub import AudioSegment
 
 
 def find_files(directory, pattern='**/*.wav'):
@@ -10,7 +12,6 @@ def find_files(directory, pattern='**/*.wav'):
     :param pattern: extension of the files
     :return: Generator via files
     '''
-    """."""
     return glob(os.path.join(directory, pattern), recursive=True)
 
 
@@ -25,7 +26,105 @@ def create_overlapping_signal(signal1, signal2, read=False):
     speech1 = signal1
     speech2 = signal2
     if read:
-        speech1 = pydub.AudioSegment.from_wav(signal1)
-        speech2 = pydub.AudioSegment.from_wav(signal2)
+        speech1 = AudioSegment.from_wav(signal1)
+        speech2 = AudioSegment.from_wav(signal2)
 
     return speech1.overlay(speech2)
+
+
+def create_overlapping_dataset(
+        input_folder,
+        output_folder,
+        size=None,
+        speech_time=2,
+        silence_time=1,
+        speakers_number=2,
+        overlapping=False,
+        pattern='**/*.wav'
+):
+    '''
+    :param input_folder:
+    :param output_folder:
+    :param size: Number of examples
+    :param speech_time:
+    :param silence_time: Time of silence which need to add between speakers
+    :param speakers_number: How many speakers need to combine inro one example
+    :param overlapping: Mix some speakers into one period of time
+    :param pattern:
+    :return: Dictionary with information about examples
+    '''
+
+    file_names = find_files(input_folder, pattern)
+
+    already_added = set()
+    files = []
+    for file_name in file_names:
+        user_id = file_name.split('/')[-3]
+        if user_id not in already_added:
+            files.append(file_name)
+            already_added.add(user_id)
+
+    perms = permutations(files, speakers_number)
+
+    result = {}
+    time = speech_time * 1000
+    silence = AudioSegment.silent(int(silence_time * 1000))
+    counter = 0
+    for speakers in perms:
+        if size and counter > size:
+            break
+
+        counter += 1
+
+        # Read wav files and put them into list as tuple (speaker_id, source data)
+        speech = []
+        text_key = ''
+        check_len = True
+        for wav_path in speakers:
+            speaker_id = wav_path.split('/')[-3]
+            source_data = AudioSegment.from_wav(wav_path)
+            speech.append((speaker_id, source_data))
+            text_key += speaker_id if not len(text_key) else '_{0}'.format(speaker_id)
+            if len(source_data) + 20 < time:
+                check_len = False
+                break
+
+        # If any file has len shorter than needed, discard this example
+        if not check_len:
+            continue
+
+        # Init info in log in ground truth file
+        if text_key not in result:
+            result[text_key] = {
+                item[0]: [] for item in speech
+            }
+
+        # Put piece of speech in the example
+        example = None
+        for item in speech:
+            if example:
+                example += silence + item[1][10:time + 10]
+            else:
+                example = item[1][10:time + 10]
+
+            result[text_key][item[0]].append((10, time + 10))
+
+        # Add piece of overlapping speech
+        if overlapping:
+            example = example + silence + \
+                      (
+                          create_overlapping_signal(speech[0][1], speech[1][1])
+                      )[0:time]
+
+        file_path = os.path.join(
+            output_folder,
+            '{0}.wav'.format(text_key)
+        )
+        example.export(file_path, format='wav')
+
+    if len(result.keys()):
+        with open(os.path.join(output_folder, 'result.pickle'), 'wb') as file_result:
+            # Pickle the 'data' dictionary using the highest protocol available.
+            pickle.dump(result, file_result, pickle.HIGHEST_PROTOCOL)
+
+    return result
