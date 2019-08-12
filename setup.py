5fba4da4d55c698e91fc995633815d00c2116b1f
Eugene Ilyushin <eugene.ilyushin@gmail.com>
2019-08-12 12:16:24 +0300

    Updated the setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="signal-transformation",
+    version="1.0.0",
+    author="Eugene Ilyushin",
+    author_email="eugene.ilyushin@gmail.com",
+    description="The package allows performing a transformation of an audio signal using TensorFlow or LibROSA",
+    long_description="The package allows performing a transformation of an audio signal using TensorFlow or LibROSA",
+    long_description_content_type="text/markdown",
+    url="https://github.com/Ilyushin/signal-transformation",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
