21c57f4b4d86f87894cf32adcda4c822d18916d6
Eugene Ilyushin <eugene.ilyushin@gmail.com>
2019-08-28 15:29:33 +0300

    Fixed local build

--- build_local.sh
+++ build_local.sh
@@ -4,4 +4,4 @@
 rm -rf ./signal_transformation.egg-info ./dist ./build
 python3 setup.py sdist bdist_wheel
 pip3 uninstall -y signal-transformation
-pip3 install ./dist/signal_transformation-1.0.18-py3-none-any.whl
+pip3 install ./dist/signal_transformation-*-py3-none-any.whl
