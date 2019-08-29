2ea012d3998196ebc4f101d79b52587caa120d1f
Eugene Ilyushin <eugene.ilyushin@gmail.com>
2019-08-29 16:40:35 +0300

    Fixed requirements

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="signal_transformation",
-    version="1.1.2",
+    version="1.1.3",
     author="Eugene Ilyushin",
     author_email="eugene.ilyushin@gmail.com",
     description="The package allows performing a transformation of an audio signal using TensorFlow or LibROSA",
@@ -23,7 +23,6 @@ setuptools.setup(
     install_requires=[
         'numpy',
         'pydub',
-        'tensorflow',
         'pylint',
         'librosa',
         'ffmpeg',
