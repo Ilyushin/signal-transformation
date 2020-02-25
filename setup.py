a677f57d264cee07ae74cf5b948d2cd7dd0334b6
Eugene Ilyushin <john.ilyushin@gmail.com>
2020-02-25 15:30:06 +0300

    Up a version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="signal_transformation",
-    version="2.0.0",
+    version="2.0.1",
     author="Eugene Ilyushin",
     author_email="eugene.ilyushin@gmail.com",
     description="The package allows performing a transformation of an audio signal using TensorFlow or LibROSA",
