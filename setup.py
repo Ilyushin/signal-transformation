d9e77141f859f819253ff6f7988a2c8ab7b94c6a
Eugene Ilyushin <e.ilyushin@cs.msu.ru>
2021-02-22 15:43:29 +0300

    Added an extension 'tfrecords'

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("requirements.txt", "r") as fh:
 
 setuptools.setup(
     name="signal_transformation",
-    version="2.2.0",
+    version="2.2.1",
     author="Eugene Ilyushin",
     author_email="eugene.ilyushin@gmail.com",
     description="The package allows performing a transformation of an audio signal using TensorFlow or LibROSA",
